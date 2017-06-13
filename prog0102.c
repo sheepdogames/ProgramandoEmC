2bee016b25de0d854062d25f578c3c794f1bdb03
Antony Gabriel <sheepdogames120@gmail.com>
2017-06-13 18:32:40 -0400

    primeiro programa em c e suas funçoes

---
+++ prog0102.c
@@ -0,0 +1,5 @@
+#include <stdio.h>          // <- Inclui também as funções de Entrada e Saida
+main()                      // <- O programa começa aqui
+{                           // <- Início de Bloco de Instruções
+    printf("Hello World");  // <- Escrever a string "Hello World" usando a função printf
+}                           // <- Fim do Bloco de Instruções (e fim de programa)
