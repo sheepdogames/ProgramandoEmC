698018da19591c128c782ecb54432bf32b337047
Antony Gabriel <sheepdogames120@gmail.com>
2017-07-09 21:33:26 -0400

    apresentando caracteres especiais em c

---
+++ prog0108.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Programa que apresenta os Caractres Especiais \n\n");
+    printf("\\n\t-\t<ENTER>\n"); /* Saida:  \n      -        <ENTER>*/
+    printf("\\\\\t-\t\\\n"); /* Saida:  \\          -       \*/
+    printf("\\t\t-\t<TAB>\n"); /* Saida: \t         -       <TAB>*/
+    printf("%%%%\t-\t%%\n");/* Saida: %%            -       %*/
+}
